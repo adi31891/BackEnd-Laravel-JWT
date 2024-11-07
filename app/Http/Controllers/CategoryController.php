4286d4c696e9da9a21d823c21fed4176a23c55f1
adi31891 <putper.siregar@gmail.com>
2024-11-07 14:33:45 +0700

    show category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -20,4 +20,38 @@ class CategoryController extends Controller
             'data' => $categories,
         ]);
     }
+
+    public function show($categorySlug)
+    {
+        $category = Category::where('slug', $categorySlug)->first();
+
+        if ($category)
+        {
+            $articles = Category::find($category->id)
+                ->articles()
+                ->with(['category' , 'user:id,name,picture'])
+                ->select([
+                    'id', 'user_id', 'category_id', 'title', 'slug', 'content_preview','featured_image', 'created_at', 'updated_at',
+                ])
+                ->paginate();
+            
+            return response()->json([
+                'meta' => [
+                    'code' => '200',
+                    'status' => 'success',
+                    'message' => 'Category retrieved successfully',
+                ],
+                'data' => $articles,
+            ], 200);
+        }
+
+        return response()->json([
+            'meta' => [
+                'code' => '404',
+                'status' => 'error',
+                'message' => 'Category not found. ',
+            ],
+            'data' =>[]
+        ], 404);
+    }
 }
